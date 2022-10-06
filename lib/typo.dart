f4421ccdb1f5edf5ded659abd6c1581af63302ef
hemant <hemantkumar692@gmail.com>
2022-10-06 23:00:22 +0530

    UI created for NFT

---
+++ lib/typo.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nft_ui/colors.dart';
+
+TextStyle header = GoogleFonts.alfaSlabOne(
+  color: white,
+  fontSize: 36,
+  fontWeight: FontWeight.w400
+);
+
+TextStyle subheader = GoogleFonts.poppins(
+  color: white,
+  fontSize: 16,
+  fontWeight: FontWeight.w600
+);
